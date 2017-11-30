24d6f0551b7359548c5b60fb244037453ea02108
Ubuntu <ubuntu@ip-172-31-6-198.us-west-2.compute.internal>
2017-11-30 07:15:52 +0000

    now sends message, just needs wait function to work

--- weekly_refresh.py
+++ weekly_refresh.py
@@ -6,6 +6,7 @@ import boto3
 import time
 import json
 import logging
+import operator
 
 class WeeklyRefresh:
   def wait_until_avail():
@@ -48,11 +49,43 @@ class WeeklyRefresh:
       tos = [header['value'] for header in payload['headers'] if header['name'] in ['To','Cc','Bcc']]
       froms = [header['value'] for header in payload['headers'] if header['name'] in ['From']]
       for header in tos:
-        for recipient in [x.strip() for x in header.split(",")]: mail[email_address]['To'].add(u.scrub(recipient))
+        for recipient in [x.strip() for x in header.split(",")]:
+          r = u.scrub(recipient)
+          if r not in mail[email_address]['To']:
+            mail[email_address]['To'][r] = 0
+          mail[email_address]['To'][r] += 1
       for header in froms:
-        for recipient in [x.strip() for x in header.split(",")]: mail[email_address]['From'].add(u.scrub(recipient))
+        for recipient in [x.strip() for x in header.split(",")]:
+          r = u.scrub(recipient)
+          if r not in mail[email_address]['From']:
+            mail[email_address]['From'][r] = 0
+          mail[email_address]['From'][r] += 1
         
-    
+    sender_address=os.environ['SENDING_ADDRESS']
+    password = os.environ['PASSWORD']
+    for email_address in mail:
+      sorted_to = sorted(mail[email_address]['To'].items(), key=operator.itemgetter(1))[-5:]
+      sorted_from = sorted(mail[email_address]['From'].items(), key=operator.itemgetter(1))[-5:]
+      message_to = "\n".join(["{} emails were addressed to {}".format(v,k) for (k,v) in sorted_to])
+      message_from = "\n".join(["{} emails were sent from {}".format(v,k) for (k,v) in sorted_from])
+      
+      message = "From: %s\r\nSubject: %s\r\nTo: %s\r\n\r\n" % (sender_address,"SendVibe Weekly Report",email_address) + \
+    """Hello from SendVibe!
+
+This week's report is looking great!  Here is some of your activity for the week:
+
+From:
+{}
+
+To:
+{}
+
+Best,
+SendVibe
+
+If you would like to stop using SendVibe, you can revoke SendVibe's access to your inbox at any time by visiting <https://myaccount.google.com/permissions> and clicking "Remove Access" in the SendVibe Alpha section.
+""".format(message_from,message_to)
+      u.mail(sender_address,email_address,message,password)
 
 if __name__== "__main__":
   wr = WeeklyRefresh()
